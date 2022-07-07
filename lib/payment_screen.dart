4ccfb6b9f71f6c91970a305c70361d01fd465799
faizank09855 <fk09855@gmail.com>
2022-07-07 13:00:00 +0530

    payment screen update

--- lib/payment_screen.dart
+++ lib/payment_screen.dart
@@ -1,146 +1,235 @@
-import 'dart:ui';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:transparent/widgets/decorations.dart';
+import 'package:flutter/widgets.dart';
 
 class PaymentScreen extends StatelessWidget {
   const PaymentScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      backgroundColor: Colors.blueGrey.shade100,
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text("Unpaid"),
+        actions: [
+          CloseButton(
+            color: Colors.black87,
+          )
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        titleTextStyle: TextStyle(
+            color: Colors.black87, fontWeight: FontWeight.w700, fontSize: 22),
+      ),
       body: Column(
         children: [
-          _topCard(context),
-          _paymentOptions(context),
-        ],
-      ),
-    ));
-  }
-
-  _topCard(BuildContext context) {
-    return ClipPath(
-      clipper: customClipper(),
-      child: Container(
-        padding: EdgeInsets.only(bottom: 4),
-        height: 250,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(32))),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 11, vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  BackButton(),
-                  Text("Payment Options"),
-                  IconButton(
-                    icon: Icon(Icons.menu),
-                    onPressed: () {},
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: Stack(
-                children: [
-                  BlurWidget(
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                "https://media.istockphoto.com/photos/colorful-vegetables-and-fruits-vegan-food-in-rainbow-colors-picture-id1284690585",
-                              ),
-                              fit: BoxFit.cover)),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(12),
-                    child: Column(
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none),
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
                       children: [
-                        Center(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                "Vegetable . ",
+                        Expanded(
+                            flex: 3,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Image.network(
+                                  "https://media.istockphoto.com/photos/colorful-vegetables-and-fruits-vegan-food-in-rainbow-colors-picture-id1284690585"),
+                            )),
+                        SizedBox(width: 12),
+                        Expanded(
+                          flex: 4,
+                          child: Column(
+                            children: [
+                              const Text(
+                                "Boat Rockerz 350 On Ear Bluetooth Headphone",
+                                textAlign: TextAlign.end,
                                 style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.w700),
+                                    fontSize: 12, fontWeight: FontWeight.bold),
                               ),
-                              Text(
-                                "Qty-1",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w600),
-                              )
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Colors.black54,
+                                                offset: Offset(1, 2),
+                                                spreadRadius: 0.1,
+                                                blurRadius: 1),
+                                          ],
+                                          borderRadius:
+                                              BorderRadius.circular(3),
+                                          gradient: const LinearGradient(
+                                              colors: [
+                                                Colors.red,
+                                                Colors.black
+                                              ],
+                                              begin: Alignment.topLeft,
+                                              end: Alignment.bottomRight)),
+                                    ),
+                                    Text(
+                                      "\$ 78.24",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w700),
+                                    )
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
                         ),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                "299.00 Rs . ",
-                                style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.w700),
-                              ),
-                              Text(
-                                "10 % off",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                            ]),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.5,
-                          height: 80,
-                          child: const Text(
-                            "in front of school , Ganj Bazar  , Flat 212 , pune  4343434 Maharashtra",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w500),
-                          ),
-                        )
+                        SizedBox(width: 12),
+                        Icon(Icons.info_outline_rounded)
                       ],
                     ),
-                  )
-                ],
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text(
+                        "Use Promo Code",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black45),
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide.none),
+                        fillColor: Colors.grey.shade200,
+                        hintText: "Type Promo Code",
+                        hintStyle: const TextStyle(
+                            color: Colors.black45,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500),
+                        filled: true,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Card(
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none),
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                cardText("Boat Rockerz 350 Bluetooth Earphone"),
+                                cardText("Tax"),
+                                cardText("subtotal"),
+                                cardText("Promocode"),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                cardText("74.68"),
+                                cardText("1.25"),
+                                cardText("76.93"),
+                                cardText("-10.93"),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      thickness: 1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            "Total",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 21,
+                              color: Colors.black87,
+                              height: 1.5,
+                            ),
+                          ),
+                          Text(
+                            "\$ 66.93",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 21,
+                              color: Colors.black87,
+                              height: 1.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ) ,
+
+          Container(
+              height: MediaQuery.of(context).size.height * 0.075,
+              margin: const EdgeInsets.all(32),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), gradient:RadialGradient(colors: [Colors.deepOrange , Colors.deepOrangeAccent] ,radius: 12 ) ),
+              width: double.infinity,
+              child: ElevatedButton(child: Text("Pay Now" , style: TextStyle(color: Colors.white , fontSize: 18),),onPressed: (){},style: ElevatedButton.styleFrom(primary: Colors.transparent , elevation: 0)))
+        ],
       ),
     );
   }
 
-  _paymentOptions(BuildContext context) {
-    return Column(
-      children: [
-        _payTile("Google Pay", Colors.blue),
-        _payTile("Phone Pe", Colors.purple),
-        _payTile("PayTM", Colors.white),
-        Divider(),
-        _payTile("Cash On Delivery", Colors.amber),
-      ],
-    );
-  }
-
-  _payTile(String text, Color color) {
-    return PaymentTile(
-      color: color,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            text,
-          ),
-          Icon(Icons.arrow_forward_ios_outlined),
-        ],
+  cardText(text) {
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        fontWeight: FontWeight.w400,
+        fontSize: 13,
+        color: Colors.grey.shade600,
+        height: 2,
       ),
     );
   }
